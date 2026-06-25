4696e8d806d8f3c1b017bb260bb8ba67d4d86429
simon <simon249@hotmail.com>
2026-06-24 21:08:25 -0300

    fix new endpoints

--- app/api/payout/[seller_id]/route.ts
+++ app/api/payout/[seller_id]/route.ts
@@ -1,14 +1,18 @@
-'use client'
-import { searchAllPayoutsByUser } from "@/app/lib/actions"
-import { NextResponse } from "next/server";
 
-export async function GET({
-    params,
-    }: {
-        params: { seller_id: string };
-    }){
-    const { seller_id: seller_id } = params;
-    const response = await searchAllPayoutsByUser(seller_id)
 
-    return new NextResponse(JSON.stringify({  response  }))
+import { NextRequest, NextResponse } from "next/server";
+import { searchAllPayoutsByUser } from "@/app/lib/actions";
+
+export async function GET(
+    request: NextRequest, // 1er parámetro: La petición
+    context: { params: Promise<{ seller_id: string }> } // 2do parámetro: El contexto con los params asíncronos
+) {
+    // Await obligatorio para los parámetros en Next.js 15+
+    const { seller_id } = await context.params;
+
+    // Ejecutamos la acción de búsqueda
+    const response = await searchAllPayoutsByUser(seller_id);
+
+    // Utilizamos el método .json() que es más limpio y directo
+    return NextResponse.json({ response });
 }
